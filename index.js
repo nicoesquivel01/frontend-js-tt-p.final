e1cbfab557281255e81e981bb7a2c99111bc3f8d
Nicolas <NICOESQUIVEL01@GMAIL.COM>
2024-12-28 16:48:14 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,28 +3,28 @@ const arrayindex = [
     {
         id: 1,
         precio: "$500000",
-        img: "../imagenes productos/placas de video/2060.png",
+        img: "imagenes productos/placas de video/2060.png",
         nombre: "GTX 2060",
         boton: "Comprar"
     },
     {
         id: 2,
         precio: "$550000",
-        img: "../imagenes productos/placas de video/2060super.png",
+        img: ".imagenes productos/placas de video/2060super.png",
         nombre: "NVIDIA RTX 2060S",
         boton: "Comprar"
     },
     {
         id: 3,
         precio: "$600000",
-        img: "../imagenes productos/placas de video/1024 (1).png",
+        img: "imagenes productos/placas de video/1024 (1).png",
         nombre: "AMD RX 6800",
         boton: "Comprar"
     },
     {
         id: 4,
         precio: "$450000",
-        img: "../imagenes productos/placas de video/000000000041545231188415452--1-.png",
+        img: "imagenes productos/placas de video/000000000041545231188415452--1-.png",
         nombre: "NVIDIA RTX 4070",
         boton: "Comprar"
     },
